5645089a82ed2f6ab239877ed0a1f66cddbd3959
vatsalkudecha <13111vatsal@gmail.com>
2025-08-09 18:42:05 +0530

    ngIf directives with ng-template

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { WishItem } from './models/wishItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
 export class AppComponent {
+  items : WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', false),
+    new WishItem('2 DSA questions', false) 
+  ];
   title = 'wishlist';
 }
